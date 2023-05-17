2a2a34649ef000453e289171eacbcaa5573aa3cb
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-18 01:13:44 +0900

    웹개발 방식 3. api String, Json 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,9 +17,37 @@ public class HelloController {
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model){
-        //http://localhost:8080/hello-mvc?name=spring
+        // http://localhost:8080/hello-mvc?name=spring
         model.addAttribute("name", name);
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        // http://localhost:8080/hello-string?name=spring
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        // http://localhost:8080/hello-api?name=spring
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // {"name":"spring"}
+    }
+
+    static class Hello {
+        // 자바 빈 표준 규약, 프로퍼티 접근 방식 (getter, setter)
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
